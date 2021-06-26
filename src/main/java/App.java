15a46bfa171b9d2c803c59a3cf6f2a22d678872d
Anthony <antonio4793@hotmail.com>
2021-06-26 11:22:46 -0500

    use bean properties

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,22 @@
+import com.anthonydi.application.acl.Message;
+import com.anthonydi.application.controllers.AppConfig;
 import com.anthonydi.application.services.MovieService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext( "beans.xml" );
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext( AppConfig.class);
 
         final MovieService movieService = applicationContext.getBean( MovieService.class );
+        final Message message = applicationContext.getBean( Message.class );
 
         movieService.findMovies().forEach( movie -> System.out.println( movie.toString() ) );
+        System.out.println( message.getMessage() );
 
 //        System.out.println( bdA.equals( bdB ) );
 
 
-        ( ( ClassPathXmlApplicationContext ) applicationContext ).close();
+        ( ( AnnotationConfigApplicationContext ) applicationContext ).close();
     }
 }
--- src/main/java/com/anthonydi/application/controllers/AppConfig.java
+++ src/main/java/com/anthonydi/application/controllers/AppConfig.java
@@ -3,9 +3,13 @@ package com.anthonydi.application.controllers;
 import com.anthonydi.application.services.MovieService;
 import com.anthonydi.infraestructure.persitance.repositories.MovieRepository;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan( basePackages = "com.anthonydi" )
+@PropertySource( "classpath:/display.properties" )
 public class AppConfig {
 
     @Bean()
